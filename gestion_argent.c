b29e94f30d5feb54694943d21b2aec589f971d25
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-04-21 22:05:49 +0200

    Create gestion_argent.c
    retire l'argent des ustensiles à l'argent du cabinet

---
+++ gestion_argent.c
@@ -0,0 +1,69 @@
+int fonction_gestion_argent_cabinet(Jeu j, Dentiste d) // j variable de type Jeu d dentiste
+{
+    FILE fichier = NULL ;
+    
+    fichier = fopen ("ustensiles.txt", "r") ;
+    
+    if (fichier == NULL){
+        printf("Ouverture du fichier impossible\n");
+        printf("code d'erreur = %d \n", errno );
+        printf("Message d'erreur = %s \n", strerror(errno) );
+        exit(1);
+    }
+    
+    if (d.main == 0){
+        printf("Il faut mettre ses gants!");
+    }
+    
+    else{
+        
+        switch(d.ustensiles){
+        
+        case 1:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 2:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 3:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 4:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 5:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 6:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 7:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        case 8:
+            fscanf(fichier, "%f" , &argent_ustensile);
+            j.argent_cabinet = j.argent_cabinet - argent_ustensile;
+        break;
+        
+        default:
+        printf("Problème rencontrez !");
+        break;
+    }
+    
+    fclose(fichier) ;
+    return j;
+}
