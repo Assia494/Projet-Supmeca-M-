0effbaa76630e1eb798f4c467208401d405378d3
Catherine283 <catherine.ngono@etu.cyu.fr>
2026-04-22 18:21:17 +0200

    Update fichier_client.c

--- fichier_client.c
+++ fichier_client.c
@@ -8,4 +8,17 @@ void gestion_fichier_patient(Patient p)
 
   fichier = fopen("pathologie_client.txt", "r");
 
-  if
+  if (fichier == NULL){
+     printf("Ouverture du fichier impossible\n");
+     printf("code d'erreur = %d \n", errno );
+     printf("Message d'erreur = %s \n", strerror(errno) );
+     exit(1);
+}
+
+fprintf(fichier, "%d", &p.pathologie);
+fprintf(fichier, "%d", &p.ustensiles);
+fprintf(fichier, "%f", &p.montant_a_payer);
+
+fclose(fichier);
+
+}
